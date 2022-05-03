33b98b7b746063a15bb80b27f230ba0fa9970413
anh.pham <anh.pham081188@gmail.com>
2022-05-03 18:20:27 +0700

    implement callback when responding successfully to FE

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { nextTick } from 'process';
 
 (async () => {
 
@@ -9,9 +10,6 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-
-  // Init valid-url module
-  var validUrl = require('valid-url');
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
@@ -33,14 +31,24 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
   app.get("/filteredimage", async (req, res) => {
     let {image_url} = req.query;
-    if (!validUrl.isUri(image_url)) {
+
+    // validate the image_url query
+    if (!image_url) {
       return res.status(400).send({ message: 'URL of image is required' });
     }
     console.log(`Start downloading the image url: ${image_url}`);
     const outPath = await filterImageFromURL(image_url);
-    // deleteLocalFiles([result])
-    console.log(`The PATH: ${outPath}`);
+    
+    console.log(`The location of the file: ${outPath}`);
     res.sendFile(outPath);
+    // remove the file when res.sendFile is done
+    res.on('finish', function() {
+      try {
+        deleteLocalFiles([outPath]);
+      } catch (ex) {
+        console.log(`Error when removing ${outPath}`);
+      }
+    });
   })
   //! END @TODO1
   
