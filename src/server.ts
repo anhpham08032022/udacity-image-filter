6e86338ad853bd99f7eaac64c8865c5dd4b71a95
anh.pham <anh.pham081188@gmail.com>
2022-05-03 21:50:01 +0700

    remove redundant content

--- src/server.ts
+++ src/server.ts
@@ -14,21 +14,6 @@ import { nextTick } from 'process';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
   app.get("/filteredimage", async (req, res) => {
     let {image_url} = req.query;
 
@@ -36,11 +21,18 @@ import { nextTick } from 'process';
     if (!image_url) {
       return res.status(400).send({ message: 'URL of image is required' });
     }
+    let outPath = "";
     console.log(`Start downloading the image url: ${image_url}`);
-    const outPath = await filterImageFromURL(image_url);
+    try {
+      outPath = await filterImageFromURL(image_url);
+    } catch (error) {
+      console.log(`Got error when processing image: ${error}`);
+      return res.status(442).send({ message: 'Got Error message when processing image.' });
+    }
     
     console.log(`The location of the file: ${outPath}`);
     res.sendFile(outPath);
+
     // remove the file when res.sendFile is done
     res.on('finish', function() {
       try {
